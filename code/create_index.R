26cdac4e51975f1d9f23ed233fd9d0ff74776a0f
Michelle Stuart <bunx21@yahoo.com>
2016-10-25 12:32:21 -0400

    working version

--- code/create_index.R
+++ code/create_index.R
@@ -1,18 +1,20 @@
 # The purpose of this code is to create an index file for use in sequence analysis by matching the Pool number to the Illumina index code and the string of nucleotides used to make that index
 
-seq <- "SEQ17"  #manually fill in seq number
-
+seq <- c("SEQ17")  #manually fill in seq number
 
 # Query the database to find out which pools(PCRs) went into that seq ------
 # connect to the database
 suppressMessages(library(dplyr))
 labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
 
-# get the pools contained in this seq
-pools <- labor %>% tbl("sequencing") %>% filter(SEQ_ID == "SEQ17") %>% select(contents) %>% collect()
+suppressWarnings(index <- labor %>% tbl("pcr") %>% select(index, pool_id) %>% filter(SEQ == seq))
 
-# split pools into a list of 4 values
-pools <- strsplit(as.character(pools), ", ")
+illumina <- labor %>% tbl("illumina")
 
-# find the index number for each pool
-pcr <- labor %>% tbl("pcr")  %>% filter(pcr_id %in% pools)
+# match the index number to the nucleotide code
+index <- left_join(index, illumina, by = c("index" = "index_num")) %>% collect()
+
+index <- index[ , c(2,4)]
+
+# write a tsv of the result - this should be uploaded to amphiprion
+write.table(index, file = "index-seq17.tsv", sep = "\t", row.names = F, col.names = F, quote = F)
