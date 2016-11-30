ce8658de901bfa4ee9d6d0154eb90240081fe80c
Michelle Stuart <bunx21@yahoo.com>
2016-11-30 15:29:14 -0500

    stuff

--- identity/issueresolver.R
+++ identity/issueresolver.R
@@ -13,7 +13,7 @@ leyte <- src_mysql(dbname = "Leyte", default.file = path.expand("~/myconfig.cnf"
 iss <- leyte %>% tbl("known_issues") %>% collect()
 
 # issue under examination
-lig <- "L0737"
+lig <- "L1292"
 
 sample <- findsample(lig)
 lab <- findlab(sample$sample_id)
@@ -27,8 +27,20 @@ print(paste(lab$ligation_id[1], "matches with", idcsv$Second.ID[which(idcsv$Firs
 Y <- lab$ligation_id[2]
 print(paste(lab$ligation_id[2], "matches with", idcsv$Second.ID[which(idcsv$First.ID == Y)], idcsv$First.ID[which(idcsv$Second.ID == Y)]))
 
+Z <- lab$ligation_id[3]
+print(paste(lab$ligation_id[3], "matches with", idcsv$Second.ID[which(idcsv$First.ID == Z)], idcsv$First.ID[which(idcsv$Second.ID == Z)]))
+
+A <- lab$ligation_id[4]
+print(paste(lab$ligation_id[4], "matches with", idcsv$Second.ID[which(idcsv$First.ID == A)], idcsv$First.ID[which(idcsv$Second.ID == A)]))
+
+B <- lab$ligation_id[5]
+print(paste(lab$ligation_id[5], "matches with", idcsv$Second.ID[which(idcsv$First.ID == B)], idcsv$First.ID[which(idcsv$Second.ID == B)]))
+
 reads$retained[which(reads$ligation_id == X)]
 reads$retained[which(reads$ligation_id == Y)]
+reads$retained[which(reads$ligation_id == Z)]
+reads$retained[which(reads$ligation_id == A)]
+reads$retained[which(reads$ligation_id == B)]
 
 # Where was the fish caught?
 site1 <- which(idcsv$Second.ID == lig)
@@ -59,74 +71,24 @@ if (nrow(visits) > 1){
 }else{print("This was the only visit made to this anemone.")}
 
 # other anemones in the area have what fish?
-anem2 <- 133
+anem2 <- 226
 oanem <- leyte %>% tbl("anemones") %>% filter(anem_id == anem2) %>% select(anem_table_id) %>% as.list(collect())
 print(oanem)
-ati <- 6470
+ati <- 2363
 atable2 <- leyte %>% tbl("clownfish") %>% filter(anem_table_id == ati) %>% select(sample_id, size, col) %>% collect()
 print(atable2)
 
+# pull labwork plate locations
+extract <- lab$extraction_id
+digest <- lab$digest_id
+ligation <- lab$ligation_id
 
-
-
-
-
-
-# Before 11-28-2016
-# ##########################################################################################
-# # Open the csv from identity analysis and look for resolutions to issues
-# 
-# filename <- "data/2016-11-28_idanalyis.csv"
-# 
-# dat <- read.csv(filename, stringsAsFactors = F)
-# 
-# # z <- nrow(dat) #366
-# 
-# 
-# # # find regenotyped samples
-# # regeno <- dat[dat$First.sample_id == dat$Second.sample_id, ]
-# # y <- nrow(regeno) #48
-# 
-# # # remove regenotyped samples from dat
-# # dat <- dat[dat$First.sample_id != dat$Second.sample_id, ]
-# # nrow(dat) == z-y
-# 
-# # find the samples that are on the known issues list
-# suppressMessages(library(dplyr))
-# leyte <- src_mysql(dbname = "Leyte", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
-# 
-# # 93 known issues
-# iss <- leyte %>% tbl("known_issues") %>% collect()
-# 
-# # which samples are in both the dat and iss?
-# issue1 <- data.frame()
-# issue2 <- data.frame()
-# for (i in 1:nrow(iss)){
-#   issue1 <- rbind(issue1, dat[dat$First.ID == iss$Ligation_ID[i],])
-# }
-# iss <- anti_join(iss, issue1, by = c("Ligation_ID" = "First.ID"))
-# 
-# for (i in 1:nrow(iss)){
-#   issue2 <- rbind(issue2, dat[dat$Second.ID == iss$Ligation_ID[i],])
-# }
-# iss <- anti_join(iss, issue2, by = c("Ligation_ID" = "Second.ID"))
-# 
-# 
-# issue <- rbind(issue1, issue2)
-# rm(issue1, issue2)
-# 
-# # cut down to just essential columns
-# issue <- issue[ , c("First.sample_id", "Second.sample_id", "First.ID", "Second.ID")]
-# 
-# # restore iss to full info
-# iss <- leyte %>% tbl("known_issues") %>% collect()
-# 
-# # attach notes to iss
-# iss$notes <- NA
-# 
-# iss$notes[iss$Ligation_ID == "L0370"] <- "APCL13_351 still matched to APCL14_445 when 445 was regenotyped from a new digest, APCL13_351 redigest was unsuccessful, not sequenced"
-# iss$notes[iss$Ligation_ID == "L0377"] <- "APCL13_362 still matched to APCL14_555 when 555 was regenotyped from a new digest, APCL13_351 redigest was unsuccessful, not sequenced"
-# iss$notes[iss$Ligation_ID == "L0288"] <- "APCL13_255 redigest was unsuccessful, not sequenced"
-# iss$notes[iss$Ligation_ID == "L0413"] <- "APCL13_040 redigest was unsuccessful, not sequenced"
-# iss$notes[iss$Ligation_ID == "L0415"] <- "L0415 matched to L2935 in the July 18, 2016 identity analysis, but did not match to it in the 11-18-2016 analysis because the matching loci threshold went up"
-# 
+for (i in length(extract)){
+  print(labor %>% tbl("extraction") %>% filter(extraction_id %in% extract) %>% select(extraction_id, date, well, plate))
+}
+for (i in length(digest)){
+  print(labor %>% tbl("digest") %>% filter(digest_id %in% digest) %>% select(digest_id, date, well, plate))
+}
+for (i in length(ligation)){
+  print(labor %>% tbl("ligation") %>% filter(ligation_id %in% ligation) %>% select(ligation_id, date))
+}
